785cbce43478cc25aa4ddd0ea676e581909b427f
pointW <199410075@qq.com>
2016-04-18 13:49:29 +0800

    Initial Commit

---
+++ TINYCompiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TINYCompiler
+//
+//  Created by 王点 on 16/4/18.
+//  Copyright © 2016年 wd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
